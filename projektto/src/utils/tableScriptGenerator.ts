b710668796ac7d5f83e26145f585ef57ed37c73d
marcinbonar <marcinb975@gmail.com>
2024-06-17 12:34:14 +0200

    postępy pracy

--- projektto/src/utils/tableScriptGenerator.ts
+++ projektto/src/utils/tableScriptGenerator.ts
@@ -1,29 +1,25 @@
-import { TElement } from '../types/node';
 import { Node } from 'reactflow';
+import { TElement } from '../types/node';
 import { DatabaseType } from '../types/database';
-import sortTablesByDependencies from './sortTablesByDependencies';
-import sortTablesByInheritance from './sortTablesByInheritance';
 
-
+// Pomocnicza funkcja do wyświetlania konfiguracji kolumny
 export const displayConfigElement = (value: boolean, displayValue: 'NOT NULL' | 'UNIQUE' | 'DEFAULT' | 'PRIMARY KEY') => {
   return value ? displayValue : '';
 };
 
+// Generuje definicję pojedynczej kolumny dla tabeli relacyjnej
 export const generateRelationalPropertyElement = (column: TElement) => {
   const { name, notNull, type, unique, length, defaultValue, primaryKey } = column;
   const notNullValue = displayConfigElement(notNull, 'NOT NULL');
   const uniqueValue = unique && !primaryKey ? displayConfigElement(unique, 'UNIQUE') : '';
   const defaultDisplayValue = displayConfigElement(defaultValue, 'DEFAULT');
   const typeValue = type ? `${type}(${length})` : type;
-
   const primaryKeyValue = primaryKey ? displayConfigElement(primaryKey, 'PRIMARY KEY') : '';
-
 
   return [name, typeValue, notNullValue, uniqueValue, defaultDisplayValue, primaryKeyValue].filter(el => el).join(' ');
 };
 
-
-
+// Generuje definicję pojedynczej kolumny dla tabeli obiektowej
 export const generateObjectPropertyElement = (column: TElement) => {
   const { name, notNull, type, unique, primaryKey, foreignKey, foreignTable } = column;
   const notNullValue = displayConfigElement(notNull, 'NOT NULL');
@@ -34,43 +30,45 @@ export const generateObjectPropertyElement = (column: TElement) => {
   return [name, type, notNullValue, uniqueValue, primaryKeyValue, foreignLine].filter(el => el).join(' ');
 };
 
-
+// Generuje kod SQL dla tabel relacyjnych
 export const generateRelationalCode = (tables: Node[]): string => {
-  const sortedTables = sortTablesByDependencies(tables);
-  let sqlData = sortedTables.map(({ data: table }) => {
+  const sortedTables = tables;
+  let sqlData = [] as any;
+  let foreignKeyStatements = [] as any;
+
+  sortedTables.forEach(({ data: table }) => {
     let createTableSql = `CREATE TABLE ${table.label} (\n`;
     const columnDefinitions = table.elements.map((el: TElement) => generateRelationalPropertyElement(el)).join(',\n');
     createTableSql += `${columnDefinitions}\n);`;
+    sqlData.push(createTableSql);
 
-    // Dodanie klauzuli ALTER TABLE dla kluczy obcych
-    const foreignKeyStatements = table.elements.filter((el: any) => el.foreignKey).map((el: any) =>
-      `ALTER TABLE ${table.label} ADD FOREIGN KEY (${el.name}) REFERENCES ${el.foreignTable}(${el.foreignKey});`
-    ).join('\n');
-
-    return `${createTableSql}\n${foreignKeyStatements}`;
+    // Dodanie instrukcji ALTER TABLE dla kluczy obcych na końcu
+    table.elements.filter((el: TElement) => el.foreignKey).forEach((el:any) => {
+      foreignKeyStatements.push(`ALTER TABLE ${table.label} ADD FOREIGN KEY (${el.name}) REFERENCES ${el.foreignTable}(${el.foreignKey});`);
+    });
   });
 
-  return sqlData.join('\n\n');
+  return `${sqlData.join('\n\n')}\n${foreignKeyStatements.join('\n')}`;
 };
 
-
-
-
+// Generuje kod SQL dla tabel obiektowych
 export const generateObjectCode = (tables: Node[]) => {
-  const sortedTables = sortTablesByInheritance(tables);
+  const sortedTables = tables;
+  let sqlData = [] as any;
 
-  const sqlData = sortedTables.map(({ data: table }) => {
+  sortedTables.forEach(({ data: table }) => {
     let createTableSql = `CREATE TABLE ${table.label} (\n`;
     const columnDefinitions = table.elements.map((el: TElement) => generateObjectPropertyElement(el)).join(',\n');
-    const inheritsTables = table.elements.map((element: TElement) => element.foreignTable).filter(Boolean);
-    const inheritsSuffix = inheritsTables.length ? `INHERITS (${inheritsTables.join(', ')})` : '';
+    const inheritsTables = table.elements.map((el: TElement) => el.foreignTable).filter(Boolean);
+    const inheritsSuffix = inheritsTables.length ? ` INHERITS (${inheritsTables.join(', ')})` : '';
     createTableSql += `${columnDefinitions}\n)${inheritsSuffix};`;
-    return createTableSql;
+    sqlData.push(createTableSql);
   });
 
   return sqlData.join('\n\n');
 };
 
+// Funkcja główna do generowania skryptów dla określonego typu bazy danych
 export const tableScriptGenerator = (tables: Node[], databaseType: DatabaseType) => {
   const modifyFn = databaseType === DatabaseType.RELATIONAL ? generateRelationalCode : generateObjectCode;
   return modifyFn(tables);
